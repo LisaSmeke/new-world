33b6ea66fc0eb5945e48dd1882b0e2c7cf7618ed
Lisa Smeke <lisasmeke@gmail.com>
2022-11-07 15:39:26 +0100

    Select capital of country

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+//get the client
 const mysql = require('mysql');
+
+//connect to database
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,8 +13,20 @@ con.connect((err) => {
   console.log('Connection established');
 });
 
-con.query('SELECT code from country', (err, columns) => {
+con.query('SELECT code from country', (err, res) => {
   if (err) throw err;
   console.log('Data received from new_world DB');
-  console.log(columns);
+  console.log(res);
 });
+
+/* What is the capital of country X ? (Accept X from user)*/
+con.query(`SELECT capital FROM country WHERE name ='Mexico'`, (err, res) => {
+  if (err) throw err;
+  console.log(res);
+});
+
+/*List all the languages spoken in the region Y (Accept Y from user)*/
+
+/*Find the number of cities in which language Z is spoken (Accept Z from user)*/
+
+/*List all the continents with the number of languages spoken in each continent*/
