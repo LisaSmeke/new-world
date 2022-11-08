e115cae4b365047d60b870eb67de146988775fd7
Lisa Smeke <lisasmeke@gmail.com>
2022-11-08 16:09:36 +0100

    Answer Q 2 and 3

--- app.js
+++ app.js
@@ -19,26 +19,41 @@ con.execute(
 FROM new_world.country
 INNER JOIN city
 ON country.Capital = city.ID
-WHERE country.Name = 'Mexico';`,
+WHERE country.Name = 'Sweden';`,
+  (err, res) => {
+    if (err) throw err;
+    console.log('Capital received from new_world db');
+    console.log(res);
+  },
+);
+
+// 2. List all the languages spoken in the region Y (Accept Y from user)
+con.execute(
+  `SELECT DISTINCT Language
+FROM new_world.country
+INNER JOIN countrylanguage
+ON country.code = countrylanguage.CountryCode
+WHERE country.region = 'Southern Europe';`,
+  (err, res) => {
+    if (err) throw err;
+    console.log('Region languages received from new_world db');
+    console.log(res);
+  },
+);
+
+// 3. Find the number of cities in which language Z is spoken (Accept Z from user)
+con.execute(
+  `SELECT COUNT(countrylanguage.CountryCode)
+FROM new_world.city
+INNER JOIN countrylanguage
+ON city.CountryCode = countrylanguage.CountryCode
+WHERE countrylanguage.language = 'Russian';`,
   (err, res) => {
     if (err) throw err;
     console.log('Data received from new_world db');
     console.log(res);
   },
 );
-
-/*List all the languages spoken in the region Y (Accept Y from user)*/
-// 1st, SELECT country.code WHERE region = Y/Middle East
-// then, SELECT countrylanguage.language WHERE countrylanguage.countrycode = (country codes that resulted from above)
-// country.code in country.region; language in countrycode
-
-/*Find the number of cities in which language Z is spoken (Accept Z from user)*/
-// Z/Spanish
-// first find countries (country codes) where language is Z/Spanish:
-// countrylanguage.countrycode WHERE countrylanguage.language = 'Z/Spanish'
-// then, check which cities have those country codes:
-// city.countrycode = (country codes that resulted from above)
-// convert result to number
 
 /*List all the continents with the number of languages spoken in each continent*/
 // 1st, list all continents:
