1ea1d0160c039a2f19190b60b57d9573fffd7369
Lisa Smeke <lisasmeke@gmail.com>
2022-11-09 10:34:15 +0100

    Accept variables from user

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 8384;
+
 //get the client
 const mysql = require('mysql2');
 
@@ -13,50 +17,59 @@ con.connect((err) => {
   console.log('Connection established');
 });
 
-// 1. What is the capital of country X ? (Accept X from user)
+// 1. What is the capital of country X ?
+// Accept X from user
+con.execute(`SELECT @countryX := 'Poland';`);
+
 con.execute(
-  `SELECT country.Name AS country, city.name AS capital, Capital, ID
+  `SELECT country.Name AS country, city.name AS capital
 FROM new_world.country
 INNER JOIN city
 ON country.Capital = city.ID
-WHERE country.Name = 'Sweden';`,
+WHERE country.Name = @countryX;`,
   (err, res) => {
     if (err) throw err;
-    console.log('Capital received from new_world db');
+    console.log(`Selected country's capital:`);
     console.log(res);
   },
 );
 
-// 2. List all the languages spoken in the region Y (Accept Y from user)
+// 2. List all the languages spoken in the region Y
+// Accept Y from user
+let region = con.execute(`SELECT @regionY := 'Middle East';`);
+
 con.execute(
   `SELECT DISTINCT Language
 FROM new_world.country
 INNER JOIN countrylanguage
 ON country.code = countrylanguage.CountryCode
-WHERE country.region = 'Southern Europe';`,
+WHERE country.region = @regionY;`,
   (err, res) => {
     if (err) throw err;
-    console.log('Region languages received from new_world db');
+    console.log(`Languages spoken in selected region`);
     console.log(res);
   },
 );
 
-// 3. Find the number of cities in which language Z is spoken (Accept Z from user)
+// 3. Find the number of cities in which language Z is spoken
+// Accept Z from user
+
+con.execute(`SELECT @languageZ := 'Norwegian';`);
+
 con.execute(
-  `SELECT COUNT(countrylanguage.CountryCode)
+  `SELECT COUNT(countrylanguage.CountryCode) AS Cities
 FROM new_world.city
 INNER JOIN countrylanguage
 ON city.CountryCode = countrylanguage.CountryCode
-WHERE countrylanguage.language = 'Russian';`,
+WHERE countrylanguage.language = @languageZ;`,
   (err, res) => {
     if (err) throw err;
-    console.log('Data received from new_world db');
+    console.log('Number of cities where selected language is spoken:');
     console.log(res);
   },
 );
 
 // 4. List all the continents with the number of languages spoken in each continent
-
 con.execute(
   `SELECT DISTINCT Continent
 FROM new_world.country
@@ -69,6 +82,4 @@ ON country.Code = countrylanguage.CountryCode`,
   },
 );
 
-// then, for each continent, get country codes
-// then, form country codes, get language
-// check how many different language values per continent
+app.listen(port, () => console.log(`Server started on port: ${port}`));
